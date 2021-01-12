4a8df087633e45241d3b81f039b088c5fee289e7
Thomas-Bagnolati <t.bagnolati@gmail.com>
2021-01-12 14:31:43 +0100

    Create layout register

--- app/src/main/java/com/yrmt/trouvetonpote/controller/LoginActivity.kt
+++ app/src/main/java/com/yrmt/trouvetonpote/controller/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.yrmt.trouvetonpote.controller
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.yrmt.trouvetonpote.R
 
@@ -8,5 +10,9 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun onClickRegister(view: View) {
+        startActivity(Intent(this, RegisterActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/yrmt/trouvetonpote/controller/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.yrmt.trouvetonpote.controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yrmt.trouvetonpote.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
