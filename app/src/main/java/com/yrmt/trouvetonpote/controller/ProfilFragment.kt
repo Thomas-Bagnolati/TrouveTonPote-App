f91a99a56a17afca593bcf8a1d88dbc7d4bbbde0
Thomas-Bagnolati <t.bagnolati@gmail.com>
2021-01-18 14:33:23 +0100

    Create EditPasswordActivity and this click btn

--- app/src/main/java/com/yrmt/trouvetonpote/controller/ProfilFragment.kt
+++ app/src/main/java/com/yrmt/trouvetonpote/controller/ProfilFragment.kt
@@ -1,10 +1,12 @@
 package com.yrmt.trouvetonpote.controller
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.yrmt.trouvetonpote.R
 
 class ProfilFragment : Fragment() {
@@ -12,6 +14,13 @@ class ProfilFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_profil, container, false)
+        val btnEditPwd = view.findViewById<Button>(R.id.btn_edit_pwd)
+
+        // Onclick Btn EditPassword
+        btnEditPwd.setOnClickListener {
+            startActivity(Intent(context, EditPasswordActivity::class.java))
+        }
+
         return view
     }
 
